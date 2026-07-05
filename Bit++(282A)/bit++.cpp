5bb6bad4903b35674bf4f016755ea74934afa219
ankitasingh2004 <ankitasinghofficial9652@gmail.com>
2026-07-05 11:31:24 +0530

    05-07-26

--- Bit++(282A)/bit++.cpp
+++ Bit++(282A)/bit++.cpp
@@ -18,3 +18,25 @@ int main(){
     return 0;
 }
 
+//Other Way
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int x=0;
+//     for(int i=0;i<n;i++){
+//         string v;
+//         cin>>v;
+//         if(v=="X++" || v=="++X"){
+//             x++;
+//         }
+//         else {
+//             x--;
+//         }
+//     }
+//     cout<<x;
+
+//     return 0;
+// }
+
